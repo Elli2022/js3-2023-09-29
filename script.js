03c7bb39a30849598b8c3ae0687a207ee7b9226c
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2023-09-29 10:54:41 +0200

    push

---
+++ script.js
@@ -0,0 +1,19 @@
+//THE CONSTRUCTOR
+
+// class Point2d{
+//     constructor(){
+//         console.log("Here is your new instance!");
+//     }
+// }
+
+// var point = new Point2d();
+
+// typeof Point2d;
+// console.log(typeof Point2d);
+
+// -----------
+
+// Static Class Behavior
+
+
+

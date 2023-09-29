63318b06490dbf3a9c6c99877b22d58353513c01
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2023-09-29 13:32:48 +0200

    push

--- script.js
+++ script.js
@@ -11,9 +11,27 @@
 // typeof Point2d;
 // console.log(typeof Point2d);
 
-// -----------
+// ----------------------------------------------------
 
-// Static Class Behavior
+// String - arrays of characters?
 
 
+var a = [];
+a[0]=1;
+a["foobar"]=2;
+
+a.length;
+a["foobar"];
+a.foobar;
+
+
+console.log(a);
+console.log(length);
+console.log("foobar");
+console.log(a.foobar);
+
+
+//----------------------------------------------------------------
+
+//Numbers - Numeric Syntax
 
